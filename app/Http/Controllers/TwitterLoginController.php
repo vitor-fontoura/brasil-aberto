49427e22f04300a1c23dafbc430a3e48a1240ff2
Vitor Fontoura <vitor.94.fontoura@gmail.com>
2021-08-18 03:54:29 -0300

    Removing unecessary code from TwitterLoginController

--- app/Http/Controllers/TwitterLoginController.php
+++ app/Http/Controllers/TwitterLoginController.php
@@ -9,7 +9,7 @@ class TwitterLoginController extends Controller
 {
     public function redirectToProvider(Request $request)
     {
-        return Socialite::driver('facebook')->scopes(['instagram'])->asPopup()->usingGraphVersion('v11.0')->redirect();
+        return Socialite::driver('twitter')->redirect();
     }
 
     public function handleProviderCallback(Request $request)
@@ -19,7 +19,7 @@ class TwitterLoginController extends Controller
             return 'user has decline the authorization';
         } 
        
-            $user = Socialite::driver('facebook')->usingGraphVersion('v11.0')->user();
+            $user = Socialite::driver('twitter')->user();
             $token = $user->token;
             $refreshToken = $user->refreshToken;
             $id = $user->getId();
